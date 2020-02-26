627dde48d575c3ab31b857f9c9fc3332bf08ba24
vjuniorr <valdemijunior16@alu.ufc.br>
2020-02-26 19:34:47 -0300

    Criação do miolo da lista e quantos iguais

---
+++ 01_arcade_basic/Quantos iguais/main.hs
@@ -0,0 +1,15 @@
+iguais3 x y z =
+    if x == y && y == z
+        then 3
+        else if x == y || x == z || y == z
+            then 2
+            else 0 
+
+main = do
+    line1 <- getLine 
+    line2 <- getLine
+    line3 <- getLine
+    let x = read line1 :: Int
+    let	y = read line2 :: Int
+    let	z = read line3 :: Int
+    print $ iguais3 x y z
