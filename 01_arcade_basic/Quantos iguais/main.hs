9196f2817f82e7c2c7d050c49806a772faf2bb2f
vjuniorr <valdemijunior16@alu.ufc.br>
2020-02-27 09:11:23 -0300

    Removendo o warning

--- 01_arcade_basic/Quantos iguais/main.hs
+++ 01_arcade_basic/Quantos iguais/main.hs
@@ -10,6 +10,6 @@ main = do
     line2 <- getLine
     line3 <- getLine
     let x = read line1 :: Int
-    let	y = read line2 :: Int
-    let	z = read line3 :: Int
+    let y = read line2 :: Int
+    let z = read line3 :: Int
     print $ iguais3 x y z
