2aa2074076b0538b09a481be02ce2f4e428d7f59
vjuniorr <valdemijunior16@alu.ufc.br>
2020-10-05 17:02:05 -0300

    filtra, map, descompacata fold e remove lista

--- Moodle (Wladimir)/Funções de alta /main.hs
+++ Moodle (Wladimir)/Funções de alta /main.hs
@@ -5,7 +5,7 @@ concatenaFold (x:xs) = foldl(++)x xs
 
 -- inverteFold --
 inverteFold :: [Int] -> [Int]
-inverteFold xs = foldl (flip(:))[] xs 
+inverteFold xs = foldl (flip(:)) [] xs
 
 -- paridadeFold --
 paridadeFold :: [Bool] -> Bool
@@ -23,3 +23,25 @@ duplicarFold (s:str) =
                 s : filter(== s) xs ++ duplicarFold str
             else 
                 s : duplicarFold str
+
+-- filtraAplicaFold --
+filtraAplicaFold :: (a->b) -> (a->Bool) -> [a] -> [b]
+filtraAplicaFold f p xs = 
+    map f (filter p xs)
+
+-- mapFold --
+mapFold :: Foldable t1 => (t2 -> a) -> t1 t2 -> [a]
+mapFold f xs = foldr (\x z -> f x : z) [] xs   
+
+-- removeLista --
+removeLista :: (Foldable t1, Foldable t2, Eq a) => t2 a -> t1 a -> [a]
+removeLista xs ys = foldr (\x z-> if x `notElem` xs then x:z else z ) [] ys
+
+-- acertosFold --
+{- acertosFold xs ys = 
+    foldr g v [0..tam-1] -}
+
+-- descompactaFold -- 
+descompactaFold :: Foldable t => t (a1, a2) -> ([a1], [a2])
+descompactaFold xs = 
+    foldr (\(a,b) (xs,ys) -> ([a]++xs, [b]++ys)) ([],[]) xs
