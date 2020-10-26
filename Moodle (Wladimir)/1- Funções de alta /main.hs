115dfc67a874f648aa0e75b1887f0de0f10e3d1d
vjuniorr <valdemijunior16@alu.ufc.br>
2020-10-25 21:51:27 -0300

    Comentando alguns codigos

--- Moodle (Wladimir)/1- Funções de alta /main.hs
+++ Moodle (Wladimir)/1- Funções de alta /main.hs
@@ -1,17 +1,38 @@
 -- concatenaFold --
 concatenaFold :: [[Int]] -> [Int]
 concatenaFold [] = []
-concatenaFold (x:xs) = foldl(++)x xs
+concatenaFold xs = foldl(++)[] xs
+
+{- 
+    Função que vai concatenar uma lista de listas em uma unica lista 
+    O foldl vai concatenar em uma lista vazia todos os elementos de uma lista de listas
+    Então uma [[],[],[]] vai ficar -> [..]
+ -}
 
 -- inverteFold --
 inverteFold :: [Int] -> [Int]
 inverteFold xs = foldl (flip(:)) [] xs
+
+{-
+    Função que inverte uma lista de inteiros
+    O foldl vai preencher uma lista vazia com o inverso da lista xs
+    A função flip vai fazer com que o fold preencha a lista vazia com o inverso de xs
+    Então uma lista [1,2,3,4,5] -> [5,4,3,2,1]
+ -}
 
 -- paridadeFold --
 paridadeFold :: [Bool] -> Bool
 paridadeFold xs
     | length(filter (==True) xs) `mod` 2 == 0 = True 
     | otherwise = False
+
+{- 
+    Função que retorna True se minha lista de Bool tiver um numero par de True e False caso contrario
+    Eu filtro a de modo que eu pego os elementos que forem True, depois pego o tamanho da lista com esse filtro e verifico se é par 
+    Se for par retorno True 
+    Outro caso eu retorno False
+    Então [True, False, False, True, True] -> False e [True, False, False, True] -> True
+ -}
 
 -- duplicarFold -- 
 duplicarFold :: String -> String
@@ -24,19 +45,43 @@ duplicarFold (s:str) =
             else 
                 s : duplicarFold str
 
+{-
+    Função que retorna uma string com vogais repetidas
+    Eu pego a cabeça da string(s) e crio uma lista xs que contem as vogais minusculas e maiusculas
+    Verifico se s é elemento de xs, se for eu concateno s com o elemento igual de xs com o restante da string
+    Então "Ana" -> "AAnaa"
+ -}
+
 -- filtraAplicaFold --
 filtraAplicaFold :: (a->b) -> (a->Bool) -> [a] -> [b]
 filtraAplicaFold f p xs = 
     map f (filter p xs)
 
+{- 
+    Função que vai aplicar uma função em determinados elementos da minha lista 
+    Crio uma lista com um map aplicando f na lista xs que vai ser filtrada por p
+    então map (*2) (==3) [1,2,3] -> [6]
+ -}
+
 -- mapFold --
 mapFold :: Foldable t1 => (t2 -> a) -> t1 t2 -> [a]
-mapFold f xs = foldr (\x z -> f x : z) [] xs   
+mapFold f xs = foldr (\x z -> f x : z) [] xs 
+
+{-
+    Função que vai aplicar f em todos os elementos de xs
+    O foldr vai preencher a lista vazia com a função passada em cada elemento de xs
+    Então (*4) [1,2,3,4] -> [4,8,12,16]
+ -}
 
 -- removeLista --
 removeLista :: (Foldable t1, Foldable t2, Eq a) => t2 a -> t1 a -> [a]
 removeLista xs ys = foldr (\x z-> if x `notElem` xs then x:z else z ) [] ys
 
+{-
+    Função que vai remover os elementos de ys que estão em xs
+    O foldr vai percorrer os elementos de ys (x) e verificar se ele não é elemento de xs, se não for ele concatena com o restante (z) na lista vazia
+    Então [1,2,3] [1,2,3,4,5,6,7] -> [4,5,6,7]
+ -}
 -- acertosFold --
 {- acertosFold xs ys = foldr g v [0..tam-1]
 
@@ -50,3 +95,9 @@ removeLista xs ys = foldr (\x z-> if x `notElem` xs then x:z else z ) [] ys
 descompactaFold :: Foldable t => t (a1, a2) -> ([a1], [a2])
 descompactaFold xs = 
     foldr (\(a,b) (xs,ys) -> ([a]++xs, [b]++ys)) ([],[]) xs
+
+{-
+    Função que vai descompactar uma lista de tupla e concatena os primeiros elementos com todos os primeiros e segundos com todos os segundos
+    O foldr vai percorrer os elementos de xs ((a,b)) e concatena com os proximos colocando em uma lista vaiza
+    Então [(1,2),(3,4)] -> ([1,3],[2,4])
+ -}
--- Moodle (Wladimir)/2 - Listas infinitas/main.hs
+++ Moodle (Wladimir)/2 - Listas infinitas/main.hs
@@ -24,13 +24,22 @@ mescla3 (x:xs) (y:ys) (z:zs)
 collatz :: Integer -> [Integer]
 collatz x 
     | x == 1 = [1]
-    |otherwise = [x] ++ collatz (seguinte x)
+    |otherwise = x : collatz (seguinte x)
 
 seguinte :: Integer -> Integer
 seguinte 1 = 1
 seguinte n 
-    | n `mod` 2 == 0 = (n `div` 2)
+    | n `mod` 2 == 0 = n `div` 2
     | otherwise = (n*3)+1
+
+{-
+    collatz recebe um inteiro e retorna uma lista de inteiros, essa lista é determinada da seguinte maneira
+    Se o meu x passado for um numero par, o proximo numero da lista vai ser esse numero divido por 2
+    Se o meu x passado for um numero impar, o proximo numero da lista vai ser esse numero vezes 3 mais 1
+    O numero seguinte é determinado pela função seguinte e retornado para ser concatenado com x
+    É realizado esse processo varias vezes até que o numeo passado para collatz seja 1
+    Então collatz 138 -> [138,69,208,104,52,26,13,40,20,10,5,16,8,4,2,1]
+-}
 
 -- fechoKleene --
 {- fechoKleene :: [a] -> [[a]]
@@ -40,9 +49,9 @@ fechoKleene xs =  -}
 goldbach :: Int -> [(Int,Int,Int)]
 goldbach n = [(n,x,y) | x <- primesR 2 (n-1),let y = n-x, isPrime y]
 
+primesR :: p -> Int -> [Int]
 primesR a b = 
-    takeWhile (<= b) $ dropWhile (< a) $ sieve [2..]
-    where sieve (n:ns) = n:sieve [ m | m <- ns, m `mod` n /= 0 ]
+    takeWhile (<= b) $ [x | x <- [2 .. ], isPrime x]
 
 
 divisores:: Int -> Int -> [Int] -> [Int]
@@ -58,6 +67,15 @@ isPrime k
     | length(divisores k k []) == 2 = True
     | otherwise = False
 
+
+{-
+    goldbach vai retornar uma lista de (n,x,y) on de n é o numero passado e x e y são numeros primos que somados dão n
+    primesR vai pegar todos os numeros que são menos que (n-1)
+    divisores vai retornar a lista de divisores para verificar se aquele numero é primo
+    isPrime verifica se um numero é primo com base na quantidade de divisores, se o numero for primo ele entra na lista de primeR
+    Então goldbach 10 -> [(10,3,7),(10,5,5),(10,7,3)]
+-}
+
 -- primosPalindromos --
 primosPalindromo :: [Int]
 primosPalindromo = [ x | x <- [2 .. ],isPrime x, palindromo (show x) ]
@@ -68,6 +86,18 @@ palindromo x
     | x == reverse x = True
     | otherwise = False
 
+{-
+    primosPalindromo retorna uma lista infinitia de numeros primos que são palindromos
+    Usando compressão de lista faço uma lista com todos os x de [2 .. ] que são primos, verificados na função isPrime e palindromo
+    Então take 10 primosPalindromo -> [2,3,5,7,11,101,131,151,181,191]
+-}
+
 -- primosGemeos --
 primosGemeos :: [(Int, Int)]
 primosGemeos = [(y,x) | x <- [2 .. ], let y = x - 2, isPrime x, isPrime y]
+
+{-
+    primosGemeos retorna uma lista infinita de tuplas (y,x) onde y e x são primos e y = x - 2
+    Verifico se y e x são primos com a função isPrime e se forem adiciono a tupla x e y
+    Então take 10 primosGemeos -> [(3,5),(5,7),(11,13),(17,19),(29,31),(41,43),(59,61),(71,73),(101,103),(107,109)]
+-}
